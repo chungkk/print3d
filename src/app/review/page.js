c58d6670e9e9c166081d25d3c0ff405edf3ca490
Hung Vu <hungns126@gmail.com>
2024-08-19 17:24:36 +0700

    Update page.js
    fix crash

--- src/app/review/page.js
+++ src/app/review/page.js
@@ -172,7 +172,6 @@ export default function Page() {
       });
       setBooks(addCoverToBook);
       localStorage.setItem('sachOiBooks', JSON.stringify(addCoverToBook));
-      setValue(addCoverToBook);
       setOpenBook(null);
       setTempCover('');
     }
