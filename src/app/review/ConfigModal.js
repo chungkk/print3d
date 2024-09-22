144771b8e2361d6395a99397f4bc08f6384957a5
hungvu <hungvu@fortna.com>
2024-09-22 12:45:12 +0700

    fix lint

--- src/app/review/ConfigModal.js
+++ src/app/review/ConfigModal.js
@@ -1,14 +1,6 @@
 "use client";
-import React, { useEffect, useState, useMemo, useCallback } from 'react';
-import axios from 'axios';
-import Navbar from "@/components/Layout/Navbar";
-import { IoSettingsOutline } from "react-icons/io5";
-import { BiSolidCloudDownload } from "react-icons/bi";
-import Modal from '@/components/Modal';
 import Button from '@/components/Button';
-import { useDropzone } from 'react-dropzone';
-import { TransformWrapper, TransformComponent } from "react-zoom-pan-pinch";
-import { domToPng } from 'modern-screenshot';
+import Modal from '@/components/Modal';
 import { useClientMediaQuery } from '@/hooks/useClientMediaQuery';
 
 export default function ConfigModal({ isOpen, onClose, config, setConfig }) {
@@ -37,7 +29,7 @@ export default function ConfigModal({ isOpen, onClose, config, setConfig }) {
 
         <div className='mt-2'>
           <p>Chỉnh sửa tiêu đề:</p>
-          <input value={config?.title || ''} onChange={onChangeInput('title')} placeholder='About you: Books' className='border border-grey w-full p-2 mt-2' placeholder='Nhập tiêu đề' />
+          <input value={config?.title || ''} onChange={onChangeInput('title')} className='border border-grey w-full p-2 mt-2' placeholder='Nhập tiêu đề' />
         </div>
         <div className='mt-2'>
           <p>Chỉnh sửa tác giả:</p>
