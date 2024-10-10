7fc0da42435a337020a72a6f545ca55be4beb464
Chungkk <hoatiuthu@gmail.com>
2024-10-10 17:36:24 +0200

    First commit

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,27 +1,10 @@
-/** @type {import('tailwindcss').Config} */
 module.exports = {
   content: [
-    "./src/pages/**/*.{js,ts,jsx,tsx,mdx}",
-    "./src/components/**/*.{js,ts,jsx,tsx,mdx}",
-    "./src/app/**/*.{js,ts,jsx,tsx,mdx}",
+    './src/**/*.{js,ts,jsx,tsx}',
+    './public/**/*.html',
   ],
   theme: {
-    extend: {
-      backgroundImage: {
-        "gradient-radial": "radial-gradient(var(--tw-gradient-stops))",
-        "gradient-conic":
-          "conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))",
-      },
-    },
-    keyframes: {
-      scaleUp: {
-        '0%': { transform: 'scaleX(0.9)' }, // Tăng kích thước từ 0.5 theo chiều ngang
-        '100%': { transform: 'scaleX(1)' }, // Đạt kích thước hiện tại
-      },
-    },
-    animation: {
-      scaleUp: 'scaleUp 0.5s ease-out', // Bạn có thể điều chỉnh thời gian và hiệu ứng easing theo ý muốn
-    },
+    extend: {},
   },
   plugins: [],
 };
